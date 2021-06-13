c132ef0884caa1e7ce2f9d32459a128211f3d28d
Mohammad Hassan Mehrazar <m.h.mehrazar@gmail.com>
2021-06-13 17:53:31 +0430

    Display author in post list

--- app/models/posts.js
+++ app/models/posts.js
@@ -3,6 +3,9 @@
 const db = require('@database/mysql');
 
 module.exports.findAll = async () => {
-  const [posts] = await db.query('SELECT * FROM posts');
+  const [posts] = await db.query(`
+  SELECT posts.*, users.full_name
+  FROM posts
+  JOIN users ON posts.author_id = users.id`);
   return posts;
 };
