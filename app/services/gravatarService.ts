c6751f18ec13a19b626bbe4f0740a97f1744f98b
Mohammad Hassan Mehrazar <m.h.mehrazar@gmail.com>
2021-07-14 15:46:51 +0430

    refactor: Delete comments

--- app/services/gravatarService.ts
+++ app/services/gravatarService.ts
@@ -2,10 +2,6 @@
 
 const gravatar = require('gravatar');
 
-// module.exports.fetchGravatarURL = (email, options) => {
-//   return gravatar.url(email, options);
-// };
-
 module.exports = class GravatarService {
   static fetchGravatarURL(email, options) {
     return gravatar.url(email, options);
