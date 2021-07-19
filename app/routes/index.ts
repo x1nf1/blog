a16b34adbf46a59c42ed490331f941ab96a25db7
Mohammad Hassan Mehrazar <m.h.mehrazar@gmail.com>
2021-07-19 14:16:55 +0430

    Implement authorization middleware

--- app/routes/index.ts
+++ app/routes/index.ts
@@ -3,10 +3,11 @@ import { Express } from 'express';
 
 const adminRouter = require('@routes/admin');
 const authRouter = require('@routes/auth');
+const authMiddleware = require('@middlewares/auth');
 
 module.exports = class MainRouter {
   constructor(private _app: Express) {
-    this._app.use('/admin', adminRouter);
+    this._app.use('/admin', [authMiddleware], adminRouter);
     this._app.use('/auth', authRouter);
   }
 };
