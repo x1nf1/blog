346a9d0dd4ef2a32867adcefc4b526f31f2c75dc
Mohammad Hassan Mehrazar <m.h.mehrazar@gmail.com>
2021-07-20 22:35:38 +0430

    Implement author partials

--- app/controllers/front/post.js
+++ app/controllers/front/post.js
@@ -2,11 +2,17 @@
 const postsModel = require('@models/posts');
 const PostsPresenter = require('@presenters/posts');
 const postsHelpers = require('@helpers/posts');
+const usersModel = require('@models/users');
+const GravatarService = require('@services/gravatarService');
+
 module.exports.showPost = async function(req, res) {
   const post = await postsModel.fetchPostBySlug(req.params.postSlug);
   if (!post) {
     return res.redirect('/404');
   }
+  const author = await usersModel.fetchUser(post.author_id);
+  post.author = author[0];
+  post.author.gravatar = GravatarService.fetchGravatarURL(author.email);
   post.presenter = new PostsPresenter(post);
   res.renderPost('front/post', { post, helpers: postsHelpers });
 };
